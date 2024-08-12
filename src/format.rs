cf284cf1581dcde1df4d8148dd84cb3310d72888
Mario <me@marioparaschiv.com>
2024-08-12 09:23:22 +0100

    🐛 fix: freed &str

--- src/format.rs
+++ src/format.rs
@@ -28,20 +28,20 @@ pub fn format_definitions(args: &Arguments, definitions: &Vec<Definition>) -> St
 pub fn format_meaning(args: &Arguments, meaning: &Meaning) -> String {
     let mut definitions = format_definitions(args, &meaning.definitions);
 
-	let synonyms: &str = if (args.synonyms || args.full) && meaning.synonyms.len() != 0 {
+	let synonyms = if (args.synonyms || args.full) && meaning.synonyms.len() != 0 {
 		let synonyms = meaning.synonyms.join(", ");
 
-		&format!(" ({})", synonyms).bright_black().italic().to_string()
+		format!(" ({})", synonyms).bright_black().italic().to_string()
 	} else {
-		""
+		"".into()
 	};
 
-	let antonyms: &str = if (args.antonyms || args.full) && meaning.antonyms.len() != 0 {
+	let antonyms = if (args.antonyms || args.full) && meaning.antonyms.len() != 0 {
 		let antonyms = meaning.antonyms.join(", ");
 
-		&format!(" ({})", antonyms).bright_red().bold().to_string()
+		format!(" ({})", antonyms).bright_red().bold().to_string()
 	} else {
-		""
+		"".into()
 	};
 
     definitions.insert_str(
@@ -74,7 +74,7 @@ pub fn format_word(args: &Arguments, word: String) -> ColoredString {
 #[allow(unused_variables)]
 pub fn format_item(args: &Arguments, item: &Word) -> String {
 	format!(
-		"\n{word} {phonetic}\n{meanings}\n\n",
+		"\n{word} {phonetic}\n{meanings}\n",
 		word = format_word(args, item.word.to_string()),
 		phonetic = format_phonetic(args, item.phonetic.as_deref().unwrap_or("".into()).to_string()),
 		meanings = format_meanings(args, &item.meanings)
